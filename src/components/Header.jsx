b94193d85fd29dd9402ba5c045442699c7608c22
Taras <senivtar1999@gmail.com>
2019-07-31 20:19:00 +0300

    added basic components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <header>
+    Hello world
+  </header>
+);
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => (
+  <main>
+    This is body
+  </main>
+);
+
+export default Home
