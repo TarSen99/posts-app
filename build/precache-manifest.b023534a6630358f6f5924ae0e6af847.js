7a39f465149df9f65eeae400ba2d722bca874c30
Taras <senivtar1999@gmail.com>
2019-08-02 21:36:32 +0300

    added prod fold

---
+++ build/precache-manifest.b023534a6630358f6f5924ae0e6af847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ca3c2b92bad7194e5a7c6844257af3",
+    "url": "/posts-app/index.html"
+  },
+  {
+    "revision": "2d218cd7e35624529126",
+    "url": "/posts-app/static/css/main.1675da1c.chunk.css"
+  },
+  {
+    "revision": "f09ab7c20c9ba5c9860f",
+    "url": "/posts-app/static/js/2.d928061a.chunk.js"
+  },
+  {
+    "revision": "2d218cd7e35624529126",
+    "url": "/posts-app/static/js/main.c76a98e0.chunk.js"
+  },
+  {
+    "revision": "4d4972f51b4516f9d73c",
+    "url": "/posts-app/static/js/runtime~main.d84ca66f.js"
+  },
+  {
+    "revision": "79d48f8b60dfe3e00b5278f5dd739bc6",
+    "url": "/posts-app/static/media/blog-header.79d48f8b.jpeg"
+  }
+]);
