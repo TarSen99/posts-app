2a4ddbec360b61324d43558111a48f8e90e97da8
Taras <senivtar1999@gmail.com>
2019-08-02 23:07:56 +0300

    added not found page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NotFound from './components/NotFound'
 import Home from './components/Home'
 import Post from './containers/Post'
 
@@ -14,6 +15,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/posts/:id" component={Post} />
+        <Route component={NotFound} />
       </Switch>
       <Footer />
     </div>
